31d34bd22fd6a4b5a51170d9b12f614115ac7133
gunnysis <qkr133456@gmail.com>
2024-02-04 02:21:22 +0900

    docker files modifiy on last

--- rootWeb/app/Providers/RouteServiceProvider.php
+++ rootWeb/app/Providers/RouteServiceProvider.php
@@ -34,7 +34,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('rootWeb')
-                ->group(base_path('routes/rootWeb.php'));
+                ->group(base_path('routes/web.php'));
         });
     }
 }
